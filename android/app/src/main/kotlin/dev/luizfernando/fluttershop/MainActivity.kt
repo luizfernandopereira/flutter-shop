5f4eb3d13563c388ff0406ef9f54784e617ace3c
Luiz Fernando Pereira <luizfernandopereira@outlook.com.br>
2023-01-31 21:41:56 -0300

    Primeiro commit

---
+++ android/app/src/main/kotlin/dev/luizfernando/fluttershop/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.luizfernando.fluttershop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:fluttershop/screens/home/home_screen.dart';
+import 'package:fluttershop/constants.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        textTheme: Theme.of(context).textTheme.apply(
+              bodyColor: kTextColor,
+            ),
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
